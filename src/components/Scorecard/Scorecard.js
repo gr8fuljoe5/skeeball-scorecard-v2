9805d12d42b48e35e495d63ae19b8b437e1a9e7e
Joe Kanakaraj <jkanakaraj@gmail.com>
2017-01-20 22:53:32 -0500

    Updates

--- src/components/Scorecard/Scorecard.js
+++ src/components/Scorecard/Scorecard.js
@@ -2,28 +2,60 @@ import React, { Component } from 'react'
 import './Scorecard.scss'
 
 class Scorecard extends Component {
+  constructor(){
+    super()
+    this.state = {
+      totals: 0
+    }
+
+  }
   shouldComponentUpdate () {
     return false
   }
 
+  getTotals() {
+    const frames = document.querySelectorAll('.sc-frame')
+    let totalsArr = [];
+    frames.forEach(function(input){
+      if(!input.value){
+        input.value = 0;
+      }
+      totalsArr.push(parseInt(input.value));
+    })
+
+    let sum = totalsArr.reduce((a, b) => a + b, 0);
+    this.setState({
+      totals: sum
+    })
+  }
+
   render () {
+    console.log('render')
+    const getTotals = this.getTotals.bind(this)
     return (
-      <table>
+      <table className='sc-table'>
         <thead>
           <tr>
             <td>Name</td>
-            {[...Array(10)].map((x, i) =>
+            {
+              [...Array(10)].map((x, i) =>
                 <td>{ i + 1 }</td>
-            )}
+              )
+            }
             <td>Total</td>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>Joe</td>
-            {[...Array(10)].map((x, i) =>
-                <td><input/></td>
-            )}
+            {
+              [...Array(10)].map((x, i) =>
+                <td>
+                  <input type='text' className='sc-frame' onChange={getTotals} />
+                </td>
+              )
+            }
+            <td>{this.state.totals}</td>
           </tr>
         </tbody>
       </table>

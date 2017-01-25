fdf7fa5588864e59018fb33119584eb2fd6cc368
Joe Kanakaraj <jkanakaraj@gmail.com>
2017-01-24 20:25:31 -0500

    Adding Scorecard component

---
+++ src/components/scorecard/index.js
@@ -0,0 +1,44 @@
+import React, { PropTypes } from 'react'
+
+import Input from '../form_input'
+
+import './index.css'
+
+const Scorecard = (props) => {
+  const topRows = () => {
+     return [...Array(10)].map((x, i) =>
+        <th className='sc-scorecard--th'>{ i + 1 }</th>
+    )
+  }
+
+  const inputRows = () => {
+    return [...Array(10)].map((x, i) =>
+        <td>
+          <Input/>
+        </td>
+    )
+  }
+
+  return (
+    <table className='sc-scorecard'>
+      <thead>
+        <tr>
+          <th>Name</th>
+          {topRows()}
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>TEST</td>
+          {inputRows()}
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+Scorecard.propTypes = {
+
+}
+
+export default Scorecard
